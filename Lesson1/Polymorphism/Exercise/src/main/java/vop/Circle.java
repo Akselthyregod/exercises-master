8bf528fe2be9062b4c01d5b62ae926a61198c329
LasseAskholm <71758394+LasseAskholm@users.noreply.github.com>
2021-02-17 12:34:32 +0100

    Deleted lesson 1

--- Lesson1/Polymorphism/Exercise/src/main/java/vop/Circle.java
+++
@@ -1,7 +0,0 @@
-package vop;
-
-public class Circle extends Ellipse{
-    Circle(double radius) {
-        super(radius, radius);
-    }
-}
--- Lesson1/Polymorphism/Exercise/src/main/java/vop/Square.java
+++
@@ -1,9 +0,0 @@
-package vop;
-
-public class Square extends Rectangle {
-
-    public Square(double length) {
-        super(length, length);
-    }
-
-}
